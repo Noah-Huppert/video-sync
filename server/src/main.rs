ec5d1ca3648d27ba262a05be86277b50e520009b
Noah Huppert <contact@noahh.io>
2020-11-26 16:22:31 -0500

    made load config from file fn

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
 use std::fs;
+use std::io;
+use std::fmt;
+use std::convert::From;
 
 use serde_derive::Deserialize;
 use toml;
@@ -18,16 +21,54 @@ struct ServerConfig {
     port: u16,
 }
 
+/// Configuration error.
+#[derive(Debug)]
+enum ConfigError {
+    /// Error occurred while fetching configuration from the disk.
+    IO(io::Error),
+
+    /// Error occurred parsing texting to configuration.
+    Parse(toml::de::Error),
+}
+
+impl From<io::Error> for ConfigError {
+    fn from(err: io::Error) -> Self {
+        ConfigError::IO(err)
+    }
+}
+
+impl From<toml::de::Error> for ConfigError {
+    fn from(err: toml::de::Error) -> Self {
+        ConfigError::Parse(err)
+    }
+}
+
 impl Config {
     /// Load configuration from a TOML file.
-    fn load_from_file(path: &str) -> Result<Config, String> {
+    fn load_from_file(path: &str) -> Result<Config, ConfigError> {
         let contents = fs::read_to_string(path)?;
-        toml::from_str(contents)
+        let cfg = toml::from_str(&contents)?;
+        Ok(cfg)
+    }
+}
+
+/// Application error.
+#[derive(Debug)]
+enum AppError {
+    /// If the configuration fails to load.
+    Config(ConfigError),
+}
+
+impl From<ConfigError> for AppError {
+    fn from(err: ConfigError) -> Self {
+        AppError::Config(err)
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), String> {
+async fn main() -> Result<(), AppError> {
     // Load configuration
-    let cfg = Config.load_from_file("./config.toml")?;
+    let cfg = Config::load_from_file("./config.toml")?;
+
+    Ok(())
 }
